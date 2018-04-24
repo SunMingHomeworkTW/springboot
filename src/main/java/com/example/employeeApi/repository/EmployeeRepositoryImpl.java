7b2071d7268bae161a1d001054369e4960c2c086
SunMing <1229536801@qq.com>
2018-04-24 13:44:36 +0800

    创建了EmployeeRepository接口的实现类EmployeeRepositoryImpl

---
+++ src/main/java/com/example/employeeApi/repository/EmployeeRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.example.employeeApi.repository;
+
+import com.example.employeeApi.model.Employee;
+
+import java.util.List;
+
+public class EmployeeRepositoryImpl implements EmployeeRepository{
+    @Override
+    public Employee createEmployee(Employee employee) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Employee deleteEmployee(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(int id, Employee employee) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployeeById(int id) {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployeeByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+}
