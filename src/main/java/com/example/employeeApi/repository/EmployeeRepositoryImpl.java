b5ffa925b717927cc73cdbd2937e2239486c1f02
SunMing <1229536801@qq.com>
2018-04-24 13:59:25 +0800

    将EmployeeDataProvider对象作为EmployeeRepositoryImpl类的一个字段，并通过调用Employee对象的相关方法实现对Employee的增删改查

--- src/main/java/com/example/employeeApi/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/employeeApi/repository/EmployeeRepositoryImpl.java
@@ -1,37 +1,42 @@
 package com.example.employeeApi.repository;
 
 import com.example.employeeApi.model.Employee;
+import com.example.employeeApi.repository.provider.EmployeeDataProvider;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class EmployeeRepositoryImpl implements EmployeeRepository{
+    private EmployeeDataProvider employeeDataProvider = new EmployeeDataProvider();
+
     @Override
     public Employee createEmployee(Employee employee) throws Exception {
-        return null;
+        return employeeDataProvider.createEmployee(employee);
     }
 
     @Override
     public Employee deleteEmployee(int id) throws Exception {
-        return null;
+        return employeeDataProvider.deleteEmployee(id);
     }
 
     @Override
     public Employee updateEmployee(int id, Employee employee) throws Exception {
-        return null;
+        return employeeDataProvider.updateEmployee(id,employee);
     }
 
     @Override
     public Employee getEmployeeById(int id) {
-        return null;
+        return employeeDataProvider.getEmployeeById(id);
     }
 
     @Override
     public Employee getEmployeeByName(String name) {
-        return null;
+        return employeeDataProvider.getEmployeeByName(name);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        return employeeDataProvider.getEmployeeList();
     }
 }
